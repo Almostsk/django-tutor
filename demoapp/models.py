7145a456a0354a4a91a137c7cbbb1bf809b0514b
Dmytro Prushchak <almostsk@yahoo.com>
2026-03-18 14:38:22 +0200

    feat: create some models and migrations

--- demoapp/models.py
+++ demoapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    price = models.IntegerField()
+    
+class Person(models.Model): 
+    person_name = models.CharField(max_length=20) 
+    email = models.EmailField() 
+    phone = models.CharField(max_length=20) 
