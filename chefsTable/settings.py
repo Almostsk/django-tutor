7735757f70b3c464c80fd84da6c2e335ccd28de8
Dmytro Prushchak <almostsk@yahoo.com>
2026-03-19 15:48:28 +0200

    feat: add admin model

--- chefsTable/settings.py
+++ chefsTable/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a7o1)f(fw4dn+94r+zj7gkey00(k16%(*j935v^nio6&a9g(7h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- demoapp/models.py
+++ demoapp/models.py
@@ -10,3 +10,13 @@ class Person(models.Model):
     person_name = models.CharField(max_length=20) 
     email = models.EmailField() 
     phone = models.CharField(max_length=20) 
+    
+class Reservation(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    contact = models.CharField('Phone number', max_length=100)
+    time = models.TimeField()
+    count = models.IntegerField()
+    notes = models.CharField(max_length=300, blank=True)
+    
+    def __str__(self):
+        return self.name
